7fcc1b827d4d385a643d2a4ca4f617496eab9448
Gulben Emiroglu <gulben.emiroglu@gmail.com>
2025-08-08 06:52:23 +0300

    search bar stillendirmesi yapıldı.

--- weather-app/src/components/SearchBar.jsx
+++ weather-app/src/components/SearchBar.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 
 const SearchBar = ({ searchTerm, onSearchChange }) => {
     return (
-        <div>
-            <input type="text" value={searchTerm} placeholder="Şehir adı girin"
-                onChange={(e) => (onSearchChange(e.target.value))} />
+        <div className="flex justify-center mt-10">
+            <input
+                type="text"
+                value={searchTerm}
+                placeholder="Şehir adı girin"
+                onChange={(e) => (onSearchChange(e.target.value))}
+                className="px-4 py-2 w-80 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-500" />
         </div>
     )
 }
